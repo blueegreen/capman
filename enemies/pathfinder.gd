df299133adc2c9f5a917fad43fe25fd645fffcb4
blueegreen <100084909+blueegreen@users.noreply.github.com>
2024-06-11 21:52:00 +0530

    basic_enemy implement

--- enemies/pathfinder.gd
+++ enemies/pathfinder.gd
@@ -3,7 +3,20 @@ extends Node2D
 @export var max_tile_count = 100
 
 func _ready():
+	start()
+
+func start():
 	var new_tile = preload("res://enemies/pathfinder_tile.tscn").instantiate()
 	new_tile.global_position = global_position
 	add_child(new_tile)
 	new_tile.start()
+
+func reset():
+	for child in get_children():
+		if child is Timer:
+			continue
+		child.queue_free()
+	start()
+
+func _on_timer_timeout():
+	reset()
